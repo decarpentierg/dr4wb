700411e4a211ded9be8806fd30500f003ffe9e33
Gonzague de Carpentier <decarpentierg@gmail.com>
2022-12-27 18:34:05 +0100

    exp1 with offset

--- experiment1.py
+++ experiment1.py
@@ -22,24 +22,33 @@ class Experiment:
         return np.sum(gamma * self.costs) / self.true_cost
 
     def sweep_projection_dimension(self, m_values, n_samples):
-        quality_ratios = np.zeros((len(m_values), k))
+        quality_ratios = np.zeros((len(m_values), n_samples))
         for m_idx, m in tqdm(enumerate(m_values)):
             for k in range(n_samples):
                 quality_ratios[m_idx, k] = self.experiment(m)
         return quality_ratios
 
 
-def plot_quality_ratios(m_values, quality_ratios, title="Experiment 1", xlabel="Projection dimension m"):
+def plot_quality_ratios(m_values, quality_ratios, label=""):
     y_mean = np.mean(quality_ratios, axis=1)
     y_std= np.std(quality_ratios, axis=1)
     y_err = 2 * y_std / np.sqrt(quality_ratios.shape[1])
-
-    plt.rcParams.update({'font.size': 22})
-    plt.figure(figsize=(13, 8))
-    plt.errorbar(m_values, y_mean, yerr=y_err)
+    plt.errorbar(m_values, y_mean, yerr=y_err, label=label)
     plt.fill_between(m_values, y_mean - y_err, y_mean + y_err, alpha=0.3)
-    plt.grid()
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel("Ratio of solution quality")
-    plt.show()
+
+
+np.random.seed(0)
+N, D = 100, 300
+MU1 = np.random.normal(size=(N, D))
+MU2 = np.random.normal(size=(N, D))
+offset = np.zeros(D)
+offset[0] = 10
+MU3 = MU2 + offset
+exp1 = Experiment(MU1, MU2)
+exp2 = Experiment(MU1, MU3)
+M_VALUES = [3, 10, 30, 100, 300]
+K = 30
+qr1 = exp1.sweep_projection_dimension(M_VALUES, K)
+qr2 = exp2.sweep_projection_dimension(M_VALUES, K)
+# plot_quality_ratios(M_VALUES, qr1, title="Experiment 1")
+# plot_quality_ratios(M_VALUES, qr2, title="Experiment 1")
